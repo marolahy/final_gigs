9eeef800173cff45ec766910338ac5e506fd9c93
Randriamifidy Bezama Marolahy <marolahy@gmail.com>
2018-04-20 10:07:36 +0300

    add service and dump

---
+++ dump/dump.sql
@@ -0,0 +1,143 @@
+-- MySQL dump 10.13  Distrib 5.7.21, for Linux (x86_64)
+--
+-- Host: localhost    Database: gigs
+-- ------------------------------------------------------
+-- Server version	5.7.21-0ubuntu0.16.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `category`
+--
+
+DROP TABLE IF EXISTS `category`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `category` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `description` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `category`
+--
+
+LOCK TABLES `category` WRITE;
+/*!40000 ALTER TABLE `category` DISABLE KEYS */;
+INSERT INTO `category` VALUES (1,'Social Media','<p>arzaer</p>'),(2,'SEO','<p>SEO tazzaraze</p>'),(3,'design','<p>Design test</p>'),(4,'development','<p>Development test</p>');
+/*!40000 ALTER TABLE `category` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `gig_image`
+--
+
+DROP TABLE IF EXISTS `gig_image`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `gig_image` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `gig_id` int(11) DEFAULT NULL,
+  `image` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `thumb` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `IDX_A1797858FE058E5` (`gig_id`),
+  CONSTRAINT `FK_A1797858FE058E5` FOREIGN KEY (`gig_id`) REFERENCES `gigs` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `gig_image`
+--
+
+LOCK TABLES `gig_image` WRITE;
+/*!40000 ALTER TABLE `gig_image` DISABLE KEYS */;
+INSERT INTO `gig_image` VALUES (1,1,'image1.jpg',''),(2,1,'image2.jpg',''),(3,1,'image3.jpg',''),(4,1,'image4.jpg','');
+/*!40000 ALTER TABLE `gig_image` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `gigs`
+--
+
+DROP TABLE IF EXISTS `gigs`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `gigs` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `category_id` int(11) DEFAULT NULL,
+  `price` double NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `description` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `featured` tinyint(1) NOT NULL,
+  `stock` int(11) NOT NULL,
+  `selled` int(11) NOT NULL,
+  `icon_image` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `background_image` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `shortdescription` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `IDX_23033DEF12469DE2` (`category_id`),
+  CONSTRAINT `FK_23033DEF12469DE2` FOREIGN KEY (`category_id`) REFERENCES `category` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `gigs`
+--
+
+LOCK TABLES `gigs` WRITE;
+/*!40000 ALTER TABLE `gigs` DISABLE KEYS */;
+INSERT INTO `gigs` VALUES (1,1,55,'Twitter Growth','<p>The Twitter Growth gig will help you to grow your Twitter following with relevant, targeted &amp; real-life people. We use keywords and competitor pages to learn who your ideal customers would be, and then we&#39;ll use our tools to target these people and turn them into your followers. This gig works great for companies who post relevant &amp; engaging Tweets on Twitter.</p>\r\n\r\n<p><strong>Will this work for me?</strong></p>\r\n\r\n<p>Yes. This will work for any Twitter account, but we encourage you to have relevant tweets for better results. Your followers will be real people relevant to your industry, so they&#39;ll be more willing to interact with you and visit your website if you have high quality, relevant tweets.</p>\r\n\r\n<p><strong>How to get started?</strong></p>\r\n\r\n<p>Click the &#39;purchase gig&#39; button above and fill in the form with relevant information. You&#39;ll then begin to see results within 24 hours, growing in efficiency from there.</p>\r\n\r\n<p>What happens after I purchase this gig?</p>\r\n\r\n<p>After you purchase this gig, we&#39;ll begin setting up your Twitter campaign. We&#39;ll constantly monitor results and we&#39;ll tweak your campaign, adding more competitors and keywords where necessary.</p>',1,100,24,'icon-twitter.png','bg-growth.jpg','Grow your Twitter account with real & targeted followers.');
+/*!40000 ALTER TABLE `gigs` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(64) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UNIQ_1483A5E9F85E0677` (`username`),
+  UNIQUE KEY `UNIQ_1483A5E9E7927C74` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'marolahy','$2y$13$Q8tnitY.3Ij81vyBy34CKuNt.FjVKgY/ITQ1cFVQ8Vwgc4B6qERRG','marolahy@gmail.com');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-04-20 10:02:04
