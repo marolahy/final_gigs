fc011ac3c6e64b6286a8b14f1041e81f702abfec
Randriamifidy Bezama Marolahy <marolahy@gmail.com>
2018-04-27 14:06:20 +0300

    add payment per paypal

--- src/Controller/OrdersController.php
+++ src/Controller/OrdersController.php
@@ -10,6 +10,8 @@ use JMS\Payment\CoreBundle\PluginController\Result;
 use App\Entity\Order;
 use App\Entity\Gigs;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use JMS\Payment\CoreBundle\Plugin\Exception\Action\VisitUrl;
+use JMS\Payment\CoreBundle\Plugin\Exception\ActionRequiredException;
 
 /**
  * @Route("/orders")
@@ -44,11 +46,11 @@ class OrdersController extends Controller
     $form = $this->createForm(ChoosePaymentMethodType::class, null, [
         'amount'   => $order->getAmount(),
         'currency' => 'USD',
+        'default_method'  => 'paypal_express_checkout',
         'predefined_data' => [
             'paypal_express_checkout' => [
               'return_url' => $this->generateUrl('payment_complete',['orderNumber' => $order->getId()],UrlGeneratorInterface::ABSOLUTE_URL),
               'cancel_url' => $this->generateUrl('payment_cancel',['orderNumber' => $order->getId()],UrlGeneratorInterface::ABSOLUTE_URL),
-              'useraction' => 'commit',
             ]
         ],
     ]);
@@ -99,13 +101,22 @@ class OrdersController extends Controller
       $ppc = $this->get('payment.plugin_controller');
       $result = $ppc->approveAndDeposit($payment->getId(), $payment->getTargetAmount());
 
-      if ($result->getStatus() === Result::STATUS_SUCCESS) {
-          return $this->redirect($this->generateUrl('app_orders_paymentcomplete', [
-              'id' => $order->getId(),
-          ]));
-      }
+      if ($result->getStatus() === Result::STATUS_PENDING)
+      {
+        $ex = $result->getPluginException();
 
-      throw $result->getPluginException();
+        if ($ex instanceof ActionRequiredException)
+        {
+            $action = $ex->getAction();
+
+            if ($action instanceof VisitUrl)
+            {
+                return $this->redirect($action->getUrl());
+            }
+        }
+    }
+
+  throw $result->getPluginException();
 
       // In a real-world application you wouldn't throw the exception. You would,
       // for example, redirect to the showAction with a flash message informing
